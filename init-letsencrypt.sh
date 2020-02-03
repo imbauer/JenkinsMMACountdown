2a37dba0acb71ef602175a946032ef77ecc09bdf
Ivan Bauer <ivanbauer16@gmail.com>
2020-02-03 05:39:59 -0500

    Changed to staging

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,8 @@ email="vanyambauer@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  echo "Existing data found for $domains. Continue and replace existing certificate? (y/N) "
+  decision = "Y"
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
